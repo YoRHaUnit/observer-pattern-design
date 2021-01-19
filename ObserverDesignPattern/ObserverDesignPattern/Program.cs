271c0483f31392a2263c90ba007cc3d53e1e83c3
remi <remi.grall@gmail.com>
2021-01-19 18:49:04 +0100

    premier commit

---
+++ ObserverDesignPattern/ObserverDesignPattern/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverDesignPattern
+{
+
+    public interface IEditor
+    {
+        void Update(ISubscriber subject);
+    }
+
+    public interface ISubscriber
+    {
+        void Attach(IEditor observer);
+
+        void Detach(IEditor observer);
+
+        void Notify();
+    }
+
+    public class Subscriber : ISubscriber
+    {
+        public int State { get; set; } = -0;
+
+        private List<IEditor> _editors = new List<IEditor>();
+
+        public void Attach(IEditor observer)
+        {
+            Console.WriteLine("Vous vous êtes abonnés à cet éditeur");
+            this._editors.Add(observer);
+        }
+
+        public void Detach(IEditor observer)
+        {
+            this._editors.Remove(observer);
+            Console.WriteLine("\nVous vous êtes désabonné de cet éditeur.");    
+        }
+
+        public void Notify()
+        {
+            Console.WriteLine("Vous allez bientot recevoir un nouveau magazine =)");
+
+            foreach (var editor in _editors)
+            {
+                editor.Update(this);
+            }
+        }
+
+        public void SomeBusinessLogic(int i)
+        {
+            Console.WriteLine("\nL'éditeur à publié le magazin n°"+i+" .");
+
+            this.Notify();
+        }
+    }
+
+    class Editor : IEditor
+    {
+        public void Update(ISubscriber subject)
+        {
+            if ((subject as Subscriber).State < 3)
+            {
+                Console.WriteLine("ConcreteObserverA: Reacted to the event.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            var subscriber = new Subscriber();
+            var editor = new Editor();
+            subscriber.Attach(editor);
+
+
+            while (i < 5)
+            {
+                i++;
+                subscriber.SomeBusinessLogic(i);
+            }
+
+            subscriber.Detach(editor);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ObserverDesignPattern/ObserverDesignPattern/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("ObserverDesignPattern")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ObserverDesignPattern")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("3ac70a5c-317e-4eb8-a48e-65979a8e8c70")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
